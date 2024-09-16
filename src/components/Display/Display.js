e192021e3a915fd8f90b92b95f0e42700ba5d779
Mohan Kandukoori <129986025+mohan-23@users.noreply.github.com>
2024-09-16 11:49:51 +0000

    first commit

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,9 +1,7 @@
 import React, { useContext } from "react";
-import { Store } from "./components/AddExpnce/Expense";
+//import { Store } from "./components/AddExpnce/Expense";
 
 const Display = () => {
-  let data = useContext(Store);
-  console.log(data);
   return <div>Hello</div>;
 };
 
